817708e1267285c8c7e9e1649dbecbbec2679b41
kingrake <kingrake@163.com>
2015-11-02 21:36:58 +0800

    Initial Commit

---
+++ iOS Start/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Start
+//
+//  Created by Lichen King on 15/11/2.
+//  Copyright © 2015年 Lichen King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
